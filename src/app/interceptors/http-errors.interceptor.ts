093aa7c4afd3f79f98dbd39863d9eb70e7646fb0
SABIQ <ammarraneez@gmail.com>
2021-07-06 15:28:56 +0530

    sorting implemented

--- src/app/interceptors/http-errors.interceptor.ts
+++ src/app/interceptors/http-errors.interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, throwError as ObservableThrowError } from "rxjs";
 import { catchError } from 'rxjs/operators';
@@ -7,7 +7,7 @@ import { catchError } from 'rxjs/operators';
 export class HttpErrorsInterceptor implements HttpInterceptor {
     constructor() { }
 
-    intercept(req : HttpRequest<any>, next : HttpHandler) : Observable<any> {
+    intercept(req : HttpRequest<any>, next : HttpHandler) : Observable<HttpEvent<any>> {
         // pipe takes next.handle(req) and returns ObservableThrowError
         return next.handle(req).pipe(
             catchError(err => {
